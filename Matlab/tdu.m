5d735be5e1b04592a5f389a4e756c375b897fc43
jprks <jep7631@g.rit.edu>
2016-11-01 00:23:22 -0400

    Implemented Excel File Write
    I implemented code to write the calculated data to an excel file.

--- Matlab/tdu.m
+++ Matlab/tdu.m
@@ -9,14 +9,14 @@ unitless='[-]';
 %% Gas Properties of Propellant
 propellant_name = 'Nitrogen';
 specific_heat_ratio = 1.4; % 1.4 for Nitrogen = 1.67 for Xenon
-molar_mass = .028013    ; % .014006 for Nitrogen - 0.131293 for Xenon
+molar_mass = .0280134; % 0.0280134 for Nitrogen - 0.131293 for Xenon
 molar_mass_units = '[kg/mol]';
 specific_volume = 0.799; 
 specific_volume_units = '[m^3/kg]';
 
 %% Nozzle Geometry
-exit_radius = .0075; % radius at nozzle exit - 0.000628315 first order approx.
-throat_radius = exit_radius*(0.633265)^(1/2); % radius at nozzle throat - 0.0005 first order approx.
+exit_radius = .008; % radius at nozzle exit - 0.008 second order approx.
+throat_radius = exit_radius/(50)^(1/2); % radius at nozzle throat
 length_units = '[m]';
 conical_half_angle = 15; % half angle of conical nozzle, 15 degrees is optimal
 angle_units = '[deg]';
@@ -27,7 +27,7 @@ temperature_units = '[K]';
 pressure_units = '[Pa]';
 
 %% Initial Tank Conditions
-chamber_pressure = (universal_gas_constant*chamber_temperature)/(specific_volume*molar_mass); 
+chamber_pressure = 1.036*10^6; 
 pressure_units = '[Pa]';
 chamber_pressure_psi_units = '[Psi]';
 
@@ -68,7 +68,7 @@ exit_mach = solve_mach(exit_area,throat_area,specific_heat_ratio);
 
 temperature_ratio = 1+((specific_heat_ratio-1)/2)*exit_mach^2;
 pressure_ratio = temperature_ratio^((specific_heat_ratio)/(specific_heat_ratio-1));
-area_ratio = throat_area/exit_area;
+area_ratio = exit_area/throat_area;
 
 exit_temperature = chamber_temperature/temperature_ratio;
 
@@ -82,7 +82,7 @@ system_delta_velocity_units = '[m/s]';
 final_system_mass = 2.07;
 initial_system_mass = 2.66;
 tank_radius = 0.025;
-tank_radius_units = '[m]';
+meter_units = '[m]';
 
 exit_pressure = chamber_pressure/pressure_ratio;
 propellant_mass_units = '[kg]';
@@ -102,7 +102,9 @@ throat_density = throat_pressure/(specific_gas_constant*throat_temperature); %[k
 throat_density_units = '[kg/m^3]';
 %   and at the throat Mach = 1 so flow rate (speed of flow) is the fluid's speed of sound at the throat
 throat_flowrate = sqrt(specific_heat_ratio*specific_gas_constant*throat_temperature);
+velocity_units = '[m/s]';
 mass_flowrate = throat_density*throat_flowrate*throat_area;
+% mass_flowrate = (specific_heat_ratio*chamber_pressure*exit_area)/sqrt(specific_heat_ratio*specific_gas_constant*chamber_temperature)*(exit_mach)*(1+(specific_heat_ratio-1)/2*(exit_mach)^2)^((2-specific_heat_ratio)/2);
 %     mass_flowrate = (specific_heat_ratio*chamber_pressure*exit_area*exit_mach)/(specific_heat_ratio*specific_gas_constant*chamber_temperature)^(1/2)*(1+(specific_heat_ratio-1)*(exit_mach)^2/2)^((2-specific_heat_ratio)/2);
 mass_flowrate_units = '[kg/s]';
 
@@ -115,6 +117,7 @@ propellant_mass = initial_system_mass - final_system_mass;
 mass_ratio = final_system_mass/initial_system_mass; % Dimensionless
 force_units = '[N]';
 isp_units='[s]';
+kilo_units = '[kg]';
 
 %% System Calculations
 
@@ -145,7 +148,7 @@ result =  {'Propellant','',propellant_name;
     'Half-angle',conical_half_angle, angle_units;
     linedivider,'','';
     'Propellant Mass',propellant_mass,propellant_mass_units;
-    'Tank Radius',tank_radius,tank_radius_units;
+    'Tank Radius',tank_radius,meter_units;
     'Mass Ratio',mass_ratio,mass_ratio_units;
     'Pressure Ratio',pressure_ratio, unitless;
     'Temperature Ratio',temperature_ratio,unitless;
@@ -158,6 +161,7 @@ result =  {'Propellant','',propellant_name;
     'Throat pressure',throat_pressure,pressure_units;
     'Mass flow rate',mass_flowrate,mass_flowrate_units;
     'Throat flowrate',throat_flowrate,mass_flowrate_units;
+    'Throat Density',throat_density,throat_density_units;
     linedivider,'','';
     'Exit temperature',exit_temperature,temperature_units;
     'Exit pressure',exit_pressure,pressure_units;
@@ -172,11 +176,22 @@ result =  {'Propellant','',propellant_name;
     'A/At',exit_area/throat_area,unitless;
     'T/Tc',exit_temperature/chamber_temperature,unitless;
     'P/Pc',exit_pressure/chamber_pressure,unitless;
-    'v/at',exit_velocity/throat_velocity,unitless;
-    };
+    'v/at',exit_velocity/throat_velocity,unitless};
 
+Tr = {'Variable','Magnitude','Unit';'Propellant',propellant_name,unitless;'Specific Heat Ratio',specific_heat_ratio,unitless;'Molar Mass',molar_mass,molar_mass_units;'Specific Gas Constant',specific_gas_constant,specific_gas_constant_units;'Chamber Temperature',chamber_temperature,temperature_units...
+    ;'Chamber Pressure',chamber_pressure,pressure_units;'Exit Radius',exit_radius,meter_units;'Throat Radius',throat_radius,meter_units;'Nozzle Length',length,meter_units;'Half Angle',conical_half_angle,...
+    unitless;'Propellant Mass',propellant_mass,kilo_units;'Spherical Tank Radius',tank_radius,meter_units;'Mass Ratio',mass_ratio,unitless;'Pressure Ratio',pressure_ratio,unitless;...
+    'Temperature Ratio',temperature_ratio,unitless;'Area Ratio',area_ratio,unitless;'Exit Area',exit_area,area_units;'Throat Area',throat_area,area_units;'Throat Temperature',throat_temperature,temperature_units...
+    ;'Throat Pressure',throat_pressure,pressure_units;'Mass Flowrate',mass_flowrate,mass_flowrate_units;'Throat Flowrate',throat_flowrate,velocity_units;'Throat Pressure',throat_pressure,pressure_units;...
+    'Exit Temperature',exit_temperature,temperature_units;'Exit Pressure',exit_pressure,pressure_units;'Thrust Force',thrust,force_units;'Exit Velocity',exit_velocity,velocity_units;...
+    'Specific Impulse',specific_impulse,isp_units;'Burn Time',burn_time,burn_time_units;'Exit Mach',exit_mach,unitless}
+
+xlswrite('Thruster Data',Tr)
 display(result);
+
 end
+
+
 
 function Mach = solve_mach(A,At,k)
 %   solve Mach number from area ratio by Newton-Raphson Method. (assume
